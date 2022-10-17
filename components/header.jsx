02bfcfa63003f404395088bf90069c4362ce7c01
David's Devel <djgm1206@gmail.com>
2022-10-17 10:19:29 -0400

    Template adaptation

---
+++ components/header.jsx
@@ -0,0 +1,15 @@
+export default function Header() {
+  return <section id="welcome-hero" className="welcome-hero">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 text-center">
+            <div className="header-text">
+              <h2>Hola <span>,</span> soy <br/> David <br/> Gonz&aacute;lez <span>.</span>   </h2>
+              <p>Desarrollador Full-Stack, en tecnologia JavaScript</p>
+              <a href="/assets/download/browney.txt" download>download resume</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+}
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,12 @@
-import '../styles/globals.css'
+import '@/styles/bootstrap.css';
+import '@/styles/bootsnav.css';
+import '@/styles/globals.css';
+import '@/styles/responsive.css';
+import '@/styles/font-awesome.min.css';
+import '@/styles/flaticon.css';
+import '@/styles/animate.css';
+import '@/styles/owl.carousel.min.css';
+import '@/styles/owl.theme.default.min.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
