0f9cc84a97ba48fd4d11ed960eb7251c466467c4
davidsdevel <davidsdevel@gmail.com>
2024-12-20 01:01:00 +0000

    chore: Updated nix file

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,10 +2,17 @@
   channel = "stable-23.11";
   packages = [
     pkgs.corepack_20
+    pkgs.openssl
   ];
   idx.extensions = [
-    
+    "Prisma.prisma"
   ];
+  services = {
+    postgres = {
+      enable = true;
+      enableTcp = true;
+    };
+  };
   idx.previews = {
   };
 }
